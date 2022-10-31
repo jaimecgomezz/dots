6f86d242f2200a9209791ba5601b561f7b9012f5
thbrd <jaimecgomezwork@gmail.com>
2022-10-30 21:39:09 -0600

    Remove Insomnia plugins

--- .config/Insomnia/plugins/insomnia-plugin-theme-nord/index.js
+++
@@ -1,8 +0,0 @@
-// For help writing plugins, visit the documentation to get started:
-//   https://support.insomnia.rest/article/173-plugins
-
-// TODO: Add plugin code here...
-module.exports.themes = [
-  require('./themes/nord-dark'),
-  require('./themes/nord-light'),
-]
