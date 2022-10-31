fa8ca23415ea93734b272d2d084b48a5b31febd9
thbrd <jaimecgomezwork@gmail.com>
2022-10-30 21:35:42 -0600

    Remove indent-blankline. Add latex.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -282,22 +282,14 @@ return require("packer").startup(function(use)
     })
 
     use({
-        "lukas-reineke/indent-blankline.nvim",
+        "lervag/vimtex",
         config = function()
-            vim.opt.list = true
+            local g = vim.g
 
-            require("indent_blankline").setup {
-                space_char_blankline = " ",
-                -- check if this makes it considerably slower
-                show_current_context = true,
-                show_current_context_start = true,
-                use_treesitter = true,
-                show_first_indent_level = false,
-                show_trailing_blankline_indent = false,
-                filetype_exclude = {
-                    "terminal", "nofile", "quickfix", "prompt", "startify"
-                }
-            }
+            g.tex_flavor = 'latex'
+            g.vimtex_view_method = 'zathura'
+            g.vimtex_quickfix_mode = 0
+            g.tex_conceal = 'abdmg'
         end
     })
 end)
